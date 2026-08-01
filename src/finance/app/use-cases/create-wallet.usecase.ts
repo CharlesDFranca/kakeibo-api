2664eb163787e33b1cfebc55e928903cfefffcf0
qin <josecharlestill@gmail.com>
2026-08-01 01:57:47 -0300

    feat: created CreateWalletUseCase

---
+++ src/finance/app/use-cases/create-wallet.usecase.ts
@@ -0,0 +1,48 @@
+import { Wallet } from 'finance/domain/entities/wallet.entity';
+import { IWalletRepository } from 'finance/domain/repositories/wallet-repository.interface';
+import { IBaseUseCase } from 'shared/app/contracts/base-usecase.contract';
+import { IUUIDGenerator } from 'shared/app/contracts/uuid-generator.contract';
+
+type CreateWalletInput = {
+    name: string;
+};
+
+type CreateWalletOutput = {
+    id: string;
+};
+
+export class CreateWalletUseCase implements IBaseUseCase<
+    CreateWalletInput,
+    CreateWalletOutput
+> {
+    constructor(
+        private readonly walletRepository: IWalletRepository,
+        private readonly idGenerator: IUUIDGenerator,
+    ) {}
+
+    async execute(input: CreateWalletInput): Promise<CreateWalletOutput> {
+        const existsByName = await this.walletRepository.existsByName(
+            input.name,
+        );
+
+        if (existsByName) {
+            throw new Error('Wallet already exists with this name');
+        }
+
+        const now = new Date();
+
+        const wallet = new Wallet(
+            this.idGenerator.generate(),
+            {
+                name: input.name,
+                balance: 0,
+            },
+            now,
+            now,
+        );
+
+        await this.walletRepository.create(wallet);
+
+        return { id: wallet.id };
+    }
+}
