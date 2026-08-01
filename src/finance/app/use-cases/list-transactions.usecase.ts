2cf4679ec24c548e70a97d78e9fc7536c9e9908f
qin <josecharlestill@gmail.com>
2026-08-01 15:05:06 -0300

    feat: create ListTransactionsUseCase

---
+++ src/finance/app/use-cases/list-transactions.usecase.ts
@@ -0,0 +1,55 @@
+import { ITransactionRepository } from 'finance/domain/repositories/transaction-repository.interface';
+import { TransactionType } from 'finance/domain/value-objects/transaction-type.vo';
+import { IBaseUseCase } from 'shared/app/contracts/base-usecase.contract';
+
+type ListTransactionsInput = void;
+
+type ListTransactionsOutput = {
+    id: string;
+    description: string;
+    amount: number;
+    date: Date;
+    type: TransactionType;
+
+    category: {
+        id: string;
+        name: string;
+    };
+
+    wallet: {
+        id: string;
+        name: string;
+    };
+}[];
+
+export class ListTransactionsUseCase implements IBaseUseCase<
+    ListTransactionsInput,
+    ListTransactionsOutput
+> {
+    constructor(
+        private readonly transactionRepository: ITransactionRepository,
+    ) {}
+
+    async execute(input: void): Promise<ListTransactionsOutput> {
+        const transactions =
+            await this.transactionRepository.findAllWithCategoryAndWallet();
+
+        return transactions.map((t) => ({
+            id: t.id,
+            description: t.description,
+            amount: t.amount,
+            date: t.date,
+            type: t.type,
+
+            category: {
+                id: t.category.id,
+                name: t.category.name,
+            },
+
+            wallet: {
+                id: t.wallet.id,
+                name: t.wallet.name,
+            },
+        }));
+    }
+}
--- src/finance/finance.module.ts
+++ src/finance/finance.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { FinanceService } from './finance.service';
-import { FinanceController } from './finance.controller';
 
 @Module({
-  controllers: [FinanceController],
-  providers: [FinanceService],
+    controllers: [],
+    providers: [],
 })
 export class FinanceModule {}
