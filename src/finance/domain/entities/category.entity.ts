27f1bdf9a2c7a482208ee0e81b3fe488d07b8768
qin <josecharlestill@gmail.com>
2026-08-01 01:31:42 -0300

    feat: create category entity

---
+++ src/finance/domain/entities/category.entity.ts
@@ -0,0 +1,33 @@
+import { BaseEntity } from 'shared/domain/entities/base-entity.entity';
+
+type CategoryProps = {
+    name: string;
+};
+
+export class Category extends BaseEntity<CategoryProps> {
+    constructor(
+        id: string,
+        props: CategoryProps,
+        createdAt: Date,
+        updatedAt: Date,
+    ) {
+        if (!props.name || props.name.trim() === '') {
+            throw new Error('Category name cannot be empty');
+        }
+
+        super(id, props, createdAt, updatedAt);
+    }
+
+    public get name(): string {
+        return this.props.name;
+    }
+
+    public rename(name: string) {
+        if (!name || name.trim() === '') {
+            throw new Error('Category name cannot be empty');
+        }
+
+        this.props.name = name;
+        this.touch();
+    }
+}
