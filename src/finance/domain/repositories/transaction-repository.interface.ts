46069232a0de427ddac727b83b0f30c179873cfe
qin <josecharlestill@gmail.com>
2026-08-01 15:04:29 -0300

    feat: add method findAllWithCategoryAndWallet in ITransactionRepository

--- src/finance/domain/repositories/transaction-repository.interface.ts
+++ src/finance/domain/repositories/transaction-repository.interface.ts
@@ -1,4 +1,23 @@
 import { Transaction } from '../entities/transaction.entity';
+import { TransactionType } from '../value-objects/transaction-type.vo';
+
+export type TransactionDetails = {
+    id: string;
+    description: string;
+    amount: number;
+    date: Date;
+    type: TransactionType;
+
+    category: {
+        id: string;
+        name: string;
+    };
+
+    wallet: {
+        id: string;
+        name: string;
+    };
+};
 
 export interface ITransactionRepository {
     create(transaction: Transaction): Promise<void>;
@@ -6,5 +25,6 @@ export interface ITransactionRepository {
     delete(id: string): Promise<void>;
     findById(id: string): Promise<Transaction | null>;
     findAll(): Promise<Transaction[]>;
+    findAllWithCategoryAndWallet(): Promise<TransactionDetails[]>;
     existsById(id: string): Promise<boolean>;
 }
