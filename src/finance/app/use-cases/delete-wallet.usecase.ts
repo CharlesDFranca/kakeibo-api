629e6d1c8c1ac8f9e69bd6af3c29543d9ce7f606
qin <josecharlestill@gmail.com>
2026-08-01 10:27:18 -0300

    feat: create DeleteWalletUseCase

---
+++ src/finance/app/use-cases/delete-wallet.usecase.ts
@@ -0,0 +1,23 @@
+import { IWalletRepository } from 'finance/domain/repositories/wallet-repository.interface';
+import { IBaseUseCase } from 'shared/app/contracts/base-usecase.contract';
+
+type DeleteWalletInput = {
+    id: string;
+};
+
+type DeleteWalletOutput = void;
+
+export class DeleteWalletUseCase implements IBaseUseCase<
+    DeleteWalletInput,
+    DeleteWalletOutput
+> {
+    constructor(private readonly walletRepository: IWalletRepository) {}
+
+    async execute(input: DeleteWalletInput): Promise<DeleteWalletOutput> {
+        if (!input.id && input.id.trim() === '') {
+            throw new Error('Error on delete.');
+        }
+
+        await this.walletRepository.delete(input.id);
+    }
+}
