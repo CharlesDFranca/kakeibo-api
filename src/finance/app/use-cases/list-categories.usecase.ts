7f51e464d3dcae484d5ce187f3cb81e5d78feec6
qin <josecharlestill@gmail.com>
2026-08-01 10:40:13 -0300

    feat: create ListCategoriesUseCase

---
+++ src/finance/app/use-cases/list-categories.usecase.ts
@@ -0,0 +1,24 @@
+import { ICategoryRepository } from 'finance/domain/repositories/category-repository.interface';
+import { IBaseUseCase } from 'shared/app/contracts/base-usecase.contract';
+
+type ListCategoriesInput = void;
+
+type ListCategoriesOutput = {
+    id: string;
+    name: string;
+}[];
+
+export class ListCategoriesUseCase implements IBaseUseCase<
+    ListCategoriesInput,
+    ListCategoriesOutput
+> {
+    constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+    async execute(input: void): Promise<ListCategoriesOutput> {
+        const categories = await this.categoryRepository.findAll();
+
+        return categories.map((c) => {
+            return { id: c.id, name: c.name };
+        });
+    }
+}
