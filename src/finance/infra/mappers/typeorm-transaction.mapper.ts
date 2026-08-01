8d8b66933b23cb1a34b392f86d11ab6af2b574d7
qin <josecharlestill@gmail.com>
2026-08-01 18:36:21 -0300

    feat: create TypeOrmTransactionMapper

---
+++ src/finance/infra/mappers/typeorm-transaction.mapper.ts
@@ -0,0 +1,37 @@
+import { TransactionEntity } from '@/database/entities/typeorm-transaction.entity';
+import { Transaction } from '@/finance/domain/entities/transaction.entity';
+import { TransactionType } from '@/finance/domain/value-objects/transaction-type.vo';
+
+export class TypeOrmTransactionMapper {
+    public toDomain(raw: TransactionEntity): Transaction {
+        return new Transaction(
+            raw.id,
+            {
+                amount: raw.amount,
+                categoryId: raw.categoryId,
+                date: raw.date,
+                description: raw.description,
+                type: new TransactionType(raw.type),
+                walletId: raw.walletId,
+            },
+            raw.createdAt,
+            raw.updatedAt,
+        );
+    }
+
+    public toPersistence(transaction: Transaction): TransactionEntity {
+        const transactionEntity = new TransactionEntity();
+
+        transactionEntity.id = transaction.id;
+        transactionEntity.amount = transaction.amount;
+        transactionEntity.description = transaction.description;
+        transactionEntity.date = transaction.date;
+        transactionEntity.type = transaction.type.value();
+        transactionEntity.categoryId = transaction.categoryId;
+        transactionEntity.walletId = transaction.walletId;
+        transactionEntity.createdAt = transaction.createdAt;
+        transactionEntity.updatedAt = transaction.updatedAt;
+
+        return transactionEntity;
+    }
+}
