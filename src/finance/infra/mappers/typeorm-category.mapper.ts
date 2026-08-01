f22a596eaf9d2e6d2e7907fe844ec9cf909eb6e3
qin <josecharlestill@gmail.com>
2026-08-01 18:44:50 -0300

    feat: create TypeOrmCategoryMapper

---
+++ src/finance/infra/mappers/typeorm-category.mapper.ts
@@ -0,0 +1,24 @@
+import { CategoryEntity } from '@/database/entities/typeorm-category.entity';
+import { Category } from '@/finance/domain/entities/category.entity';
+
+export class TypeOrmCategoryMapper {
+    public toDomain(raw: CategoryEntity): Category {
+        return new Category(
+            raw.id,
+            { name: raw.name },
+            raw.createdAt,
+            raw.updatedAt,
+        );
+    }
+
+    public toPersistence(wallet: Category): CategoryEntity {
+        const categoryEntity = new CategoryEntity();
+
+        categoryEntity.id = wallet.id;
+        categoryEntity.name = wallet.name;
+        categoryEntity.createdAt = wallet.createdAt;
+        categoryEntity.updatedAt = wallet.updatedAt;
+
+        return categoryEntity;
+    }
+}
