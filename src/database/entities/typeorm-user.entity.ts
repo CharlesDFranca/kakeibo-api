ce04b2ef3db84da0bdf3d79a96c9b3c550e4ef29
qin <josecharlestill@gmail.com>
2026-08-03 11:02:06 -0300

    feat: create UserEntity

---
+++ src/database/entities/typeorm-user.entity.ts
@@ -0,0 +1,32 @@
+import { Email } from '@/users/domain/value-objects/email.vo';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+    @PrimaryColumn()
+    id!: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    name!: string;
+
+    @Column({ type: 'varchar', unique: true })
+    email!: string;
+
+    @Column({ type: 'varchar', length: 30, unique: true })
+    username!: string;
+
+    @Column({ type: 'varchar' })
+    passwordHash!: string;
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: 'timestamptz' })
+    updatedAt!: Date;
+}
