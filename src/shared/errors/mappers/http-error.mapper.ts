b783fd97582e37e433e76f09466e5f1e27570450
qin <josecharlestill@gmail.com>
2026-08-28 12:58:21 -0300

    feat: create base errors, GlobalExceptionFilter and ResponseInterceptor

---
+++ src/shared/errors/mappers/http-error.mapper.ts
@@ -0,0 +1,41 @@
+import { ApiError } from '../formatters/response-formatters';
+import { AppError } from '../types/app-error';
+import { DomainError } from '../types/domain-error';
+import { InfraError } from '../types/infra-error';
+
+export class HttpErrorMapper {
+    static toApiError(error: unknown): ApiError {
+        if (error instanceof DomainError) {
+            return {
+                code: error.code,
+                type: 'Domain',
+                message: error.message,
+                details: error.details,
+            };
+        }
+
+        if (error instanceof AppError) {
+            return {
+                code: error.code,
+                type: 'Application',
+                message: error.message,
+                details: error.details,
+            };
+        }
+
+        if (error instanceof InfraError) {
+            return {
+                code: error.code,
+                type: 'Infrastructure',
+                message: error.message,
+                details: error.details,
+            };
+        }
+
+        return {
+            code: 'INTERNAL_ERROR',
+            type: 'Internal',
+            message: 'Internal server error.',
+        };
+    }
+}
---
+++ src/shared/errors/types/app-error.ts
@@ -0,0 +1,3 @@
+import { BaseError } from './base-error';
+
+export abstract class AppError extends BaseError {}
