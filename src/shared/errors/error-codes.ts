99b703a939b1ee271e80009761cbaebdd3fe662b
qin <josecharlestill@gmail.com>
2026-08-29 11:09:30 -0300

    feat: create InvalidTransactionDateError

--- src/shared/errors/error-codes.ts
+++ src/shared/errors/error-codes.ts
@@ -1,3 +1,5 @@
-export const ErrorCodes = {} as const;
+export const ErrorCodes = {
+    INVALID_TRANSACTION_DATE: 'INVALID_TRANSACTION_DATE',
+} as const;
 
 export type ErrorCode = (typeof ErrorCodes)[keyof typeof ErrorCodes];
