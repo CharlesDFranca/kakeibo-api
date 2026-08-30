061fcbf4df4c889b4a12142d5c5ce0640bc6c03c
qin <josecharlestill@gmail.com>
2026-08-30 11:07:09 -0300

    feat: register planning uow and facade

--- src/planning/planning.module.ts
+++ src/planning/planning.module.ts
@@ -18,6 +18,8 @@ import { TypeOrmGoalRepository } from './infra/repositories/typeorm-goal.reposit
 import { GoalMovementEntity } from './infra/entities/typeorm-goal-movement.entity';
 import { GoalEntity } from './infra/entities/typeorm-goal.entity';
 import { CoreModule } from '@/core/core.module';
+import { TypeOrmPlanningUnitOfWork } from './infra/database/typeorm-planning.uow';
+import { PlanningFacade } from './app/services/planning-facade';
 
 @Module({
     imports: [
@@ -43,10 +45,20 @@ import { CoreModule } from '@/core/core.module';
             provide: PLANNING_TOKENS.GOAL_MOVEMENT_REPOSITORY,
             useClass: TypeOrmGoalMovementRepository,
         },
+        {
+            provide: PLANNING_TOKENS.UNIT_OF_WORK,
+            useClass: TypeOrmPlanningUnitOfWork,
+        },
+        {
+            provide: PLANNING_TOKENS.FACADE,
+            useClass: PlanningFacade,
+        },
     ],
     exports: [
         PLANNING_TOKENS.GOAL_REPOSITORY,
         PLANNING_TOKENS.GOAL_MOVEMENT_REPOSITORY,
+        PLANNING_TOKENS.UNIT_OF_WORK,
+        PLANNING_TOKENS.FACADE,
     ],
 })
 export class PlanningModule {}
