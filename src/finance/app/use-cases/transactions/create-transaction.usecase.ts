b3c84c38e17e08585eb41156434026ec770210b1
qin <josecharlestill@gmail.com>
2026-08-31 11:48:31 -0300

    refactor: use unit of work in create-transaction use-case

--- src/finance/app/use-cases/transactions/create-transaction.usecase.ts
+++ src/finance/app/use-cases/transactions/create-transaction.usecase.ts
@@ -1,17 +1,15 @@
 import { ETransactionType } from '@/finance/domain/enums/transaction-type.enum';
-import type { ITransactionRepository } from '@/finance/domain/repositories/transaction-repository.interface';
-import type { IWalletRepository } from '@/finance/domain/repositories/wallet-repository.interface';
 import { FINANCE_TOKENS } from '@/finance/finance.tokens';
 import { IBaseUseCase } from '@/shared/app/contracts/base-usecase.contract';
 import type { IIDGenerator } from '@/core/app/contracts/id-generator.contract';
 import { Inject, Injectable } from '@nestjs/common';
-import { Transaction } from '../../../domain/entities/transaction.entity';
-import { TransactionType } from '../../../domain/value-objects/transaction-type.vo';
-import type { ICategoryRepository } from '@/finance/domain/repositories/category-repository.interface';
 import { Money } from '@/shared/domain/value-objects/money.vo';
+import { CORE_TOKENS } from '@/core/core.tokens';
+import { Transaction } from '@/finance/domain/entities/transaction.entity';
+import { TransactionType } from '@/finance/domain/value-objects/transaction-type.vo';
+import type { IFinanceUnitOfWork } from '../../contracts/finance-unit-of-work.contract';
 import { CategoryNotFoundError } from '../../errors/category-not-found.error';
 import { WalletNotFoundError } from '../../errors/wallet-not-found.error';
-import { CORE_TOKENS } from '@/core/core.tokens';
 
 type CreateTransactionInput = {
     userId: string;
@@ -33,59 +31,64 @@ export class CreateTransactionUseCase implements IBaseUseCase<
     CreateTransactionOutput
 > {
     constructor(
-        @Inject(FINANCE_TOKENS.TRANSACTION_REPOSITORY)
-        private readonly transactionRepository: ITransactionRepository,
-        @Inject(FINANCE_TOKENS.WALLET_REPOSITORY)
-        private readonly walletRepository: IWalletRepository,
-        @Inject(FINANCE_TOKENS.CATEGORY_REPOSITORY)
-        private readonly categoryRepository: ICategoryRepository,
         @Inject(CORE_TOKENS.ID_GENERATOR)
         private readonly idGenerator: IIDGenerator,
+        @Inject(FINANCE_TOKENS.UNIT_OF_WORK)
+        private readonly financeUow: IFinanceUnitOfWork,
     ) {}
 
     async execute(
         input: CreateTransactionInput,
     ): Promise<CreateTransactionOutput> {
-        const category = await this.categoryRepository.findUserCategoryById(
-            input.userId,
-            input.categoryId,
-        );
+        const transaction = await this.financeUow.transaction(async () => {
+            const categoryRepository = this.financeUow.getCategoryRepository();
+            const walletRepository = this.financeUow.getWalletRepository();
+            const transactionRepository =
+                this.financeUow.getTransactionRepository();
 
-        if (!category) throw new CategoryNotFoundError();
+            const category = await categoryRepository.findUserCategoryById(
+                input.userId,
+                input.categoryId,
+            );
 
-        const wallet = await this.walletRepository.findUserWalletById(
-            input.userId,
-            input.walletId,
-        );
+            if (!category) throw new CategoryNotFoundError();
 
-        if (!wallet) throw new WalletNotFoundError();
+            const wallet = await walletRepository.findUserWalletById(
+                input.userId,
+                input.walletId,
+            );
 
-        const now = new Date();
+            if (!wallet) throw new WalletNotFoundError();
 
-        const transaction = new Transaction(
-            this.idGenerator.generate(),
-            {
-                description: input.description,
-                amount: Money.fromAmount(input.amount),
-                date: input.date,
-                type: new TransactionType(input.type),
-                categoryId: category.id,
-                walletId: wallet.id,
-            },
-            now,
-            now,
-        );
+            const now = new Date();
 
-        if (transaction.isCompleted()) {
-            if (transaction.isIncome()) {
-                wallet.deposit(transaction.amount);
-            } else {
-                wallet.withdraw(transaction.amount);
+            const transaction = new Transaction(
+                this.idGenerator.generate(),
+                {
+                    description: input.description,
+                    amount: Money.fromAmount(input.amount),
+                    date: input.date,
+                    type: new TransactionType(input.type),
+                    categoryId: category.id,
+                    walletId: wallet.id,
+                },
+                now,
+                now,
+            );
+
+            if (transaction.isCompleted()) {
+                if (transaction.isIncome()) {
+                    wallet.deposit(transaction.amount);
+                } else {
+                    wallet.withdraw(transaction.amount);
+                }
             }
-        }
 
-        await this.transactionRepository.create(transaction);
-        await this.walletRepository.update(wallet);
+            await transactionRepository.create(transaction);
+            await walletRepository.update(wallet);
+
+            return transaction;
+        });
 
         return { id: transaction.id };
     }
