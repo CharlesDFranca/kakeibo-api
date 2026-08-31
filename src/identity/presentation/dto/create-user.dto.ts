919b910f10980c138834de728dadc317a6ee8377
qin <josecharlestill@gmail.com>
2026-08-31 11:27:15 -0300

    feat: create users and auth dtos

--- src/identity/presentation/dto/create-user.dto.ts
+++ src/identity/presentation/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    username!: string;
+
+    @IsEmail()
+    email!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    password!: string;
+}
